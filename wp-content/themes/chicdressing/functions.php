34bacad0aeec39c3af660a7514adcb1c61fde4bc
David BEAUMONT <dadid333@gmail.com>
2023-06-23 16:40:05 +0200

    désactivation des polices google

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -7,7 +7,12 @@ function chicdressing_enqueue_styles() {
 
 add_filter( 'big_image_size_threshold', '__return_false' );
 
+/* désactivation manuelles des polices google */
 
-
-
- 
+function remove_google_fonts_stylesheet() {
+    wp_dequeue_style('ashe-playfair-font');
+    wp_dequeue_style('ashe-opensans-font');
+    wp_dequeue_style('ashe-kalam-font');
+    wp_dequeue_style('ashe-rokkitt-font');
+  }
+  add_action('wp_enqueue_scripts', 'remove_google_fonts_stylesheet', 999);
